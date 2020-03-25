fd68d8f8842a55fda1d2d0136355b41614b61d1d
HashimotoYT <hashimoto.stream@gmail.com>
2020-03-25 21:00:49 +0200

    feat: migrate pack and resize

--- script/pack.ts
+++ script/pack.ts
@@ -1,12 +1,13 @@
-const fs = require('fs')
-const path = require('path')
-const yaml = require('js-yaml')
-const dates = require('../meta/dates.json')
-const colors = require('../meta/colors.json')
-const releases = require('../meta/releases.json')
-const readmes = require('../meta/readmes.json')
+import * as fs from 'fs'
+import * as path from 'path'
+import * as yaml from 'js-yaml'
+import * as dates from '../meta/dates.json'
+import * as colors from '../meta/colors.json'
+import * as releases from '../meta/releases.json'
+import * as readmes from '../meta/readmes.json'
+import { $TSFixMe } from '../lib/interfaces'
 const parseGitHubUrl = require('github-url-to-object')
-const apps = []
+const apps: $TSFixMe = []
 
 fs.readdirSync(path.join(__dirname, '../apps'))
 .filter(filename => {
@@ -16,22 +17,29 @@ fs.readdirSync(path.join(__dirname, '../apps'))
   const yamlFile = path.join(__dirname, `../apps/${slug}/${slug}.yml`)
   const app = Object.assign(
     {slug: slug},
-    yaml.safeLoad(fs.readFileSync(yamlFile)),
+    yaml.safeLoad(fs.readFileSync(yamlFile, { encoding: 'utf-8' })),
     {
       icon: `${slug}-icon.png`,
       icon32: `${slug}-icon-32.png`,
       icon64: `${slug}-icon-64.png`,
       icon128: `${slug}-icon-128.png`,
       icon256: `${slug}-icon-256.png`,
+      // @ts-ignore
       date: dates[slug],
+      // @ts-ignore
       iconColors: colors[slug].palette
     },
+    // @ts-ignore
     releases[slug],
+    // @ts-ignore
     readmes[slug]
   )
 
+  // @ts-ignore
   app.goodColorOnWhite = app.goodColorOnWhite || colors[slug].goodColorOnWhite
+  // @ts-ignore
   app.goodColorOnBlack = app.goodColorOnBlack || colors[slug].goodColorOnBlack
+  // @ts-ignore
   app.faintColorOnWhite = app.faintColorOnWhite || colors[slug].faintColorOnWhite
 
   // Delete website if it's the same URL as repository
